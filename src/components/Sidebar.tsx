0c9ceb0974f7e7bcdc7b0a5e8449272f02fb5c7e
Wihyeongsu <wibrother2@gmail.com>
2024-11-27 16:45:22 +0900

    Sidebar DnD 수정 진행

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -17,24 +17,21 @@ export default () => {
     event.dataTransfer.effectAllowed = "move";
   };
 
+  // Tauri api 테스트
   // 응답을 저장할 상태 추가
   const [response, setResponse] = useState<CustomResponse | null>(null);
   // 컴포넌트가 마운트될 때 Rust 함수 호출
-  useEffect(() => {
-    const callRustFunction = async () => {
-      try {
-        const res = await invoke<CustomResponse>("my_custom_command", {
-          name: "Wihyeongsu",
-        });
-        setResponse(res);
-        console.log(`Message: ${res.message}, Other Val: ${res.other_val}`);
-      } catch (e) {
-        console.error("Error calling Rust function:", e);
-      }
-    };
-
-    callRustFunction();
-  }, []); // 빈 의존성 배열로 컴포넌트 마운트 시 한 번만 실행
+  const callRustFunction = async () => {
+    try {
+      const res = await invoke<CustomResponse>("my_custom_command", {
+        name: "Wihyeongsu",
+      });
+      setResponse(res);
+      console.log(`Message: ${res.message}, Other Val: ${res.other_val}`);
+    } catch (e) {
+      console.error("Error calling Rust function:", e);
+    }
+  };
 
   return (
     <aside>
@@ -50,9 +47,21 @@ export default () => {
           {node.label}
         </div>
       ))}
-      <Button variant="outline" color="primary" style={{ marginTop: "20px" }}>
-        Generate Code {response?.message}
+      <Button
+        variant="outline"
+        color="primary"
+        style={{ marginTop: "20px" }}
+        onClick={() => {
+          callRustFunction();
+        }}>
+        Generate Code
       </Button>
+      {response && (
+        <div>
+          <p>Message: {response.message}</p>
+          <p>Other Val: {response.other_val}</p>
+        </div>
+      )}
     </aside>
   );
 };
